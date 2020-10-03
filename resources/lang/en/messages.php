b7d97be876808365153b5fc0ac01613f26d419a1
Casper Boone <mail@casperboone.nl>
2020-10-03 18:35:28 +0200

    Add Dutch intro text and refine privacy statement

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,13 +2,14 @@
 
 return [
     'welcome' => '
-        You’ve probably seen it - the rainbow coloured bridge next to Delft Station. A first visible symbol that Delft is a rainbow city, and has been for nearly 10 years. However, this bridge is only temporary. We want to create a more permanent LGBT+ monument which is more unique to Delft, and we want your help to think of what exactly!
+        You’ve probably seen it - the rainbow coloured bridge next to Delft Station. A visible symbol that Delft is a rainbow city, and has been for nearly 10 years. However, this bridge is only temporary. We want to create a more permanent LGBT+ monument, one unique to Delft, and we want your help to think of what exactly!
         <br /><br />
-        Between now and December 4th you can submit your idea for a statue or other standalone piece of art that represents the LGBT+ community. It can be just a description in words or a sketch, the winning idea will be given as part of the instructions to an artist.
-        After the competition closes, a panel of representatives of the various LGBT+ organisations in Delft will decide on the winning idea, which will be announced on Purple Friday, December 11th, after which a fundraising campaign will start to fund it. If your idea wins, you can be part of the eventual reveal of the artwork and you will receive a small version of the artwork to take home.
+        Between now and December 1st you can submit your idea for a statue or other standalone piece of art that represents the LGBT+ community. It can be a description in words or a sketch, whatever gets your idea across. We will work with a professional artist to turn the winning idea into reality.
+        <br /><br />
+        After the competition closes a panel of representatives from the various LGBT+ organisations in Delft will decide on the winning idea. This will be announced on Purple Friday, December 11th. Then a fundraising campaign will start to fund the winning idea. The winner will be invited to participate in the further development of the artwork and be part of its eventual reveal. They will also receive a smaller version of the artwork to take home.
     ',
     'form' => [
         'title' => 'Submit <span class="text-art-purple">your idea</span> for an <span class="text-art-purple">artwork</span>!',
-        'privacy' => 'By submitting this form you allow DWH to save your information and to process it. Your information will only be used to pick ideas and for the possibility to get in touch with you. After the competition has finished, your data will be removed. If you have more questions, please contact us via the information below.'
+        'privacy' => 'By submitting this form you allow DWH to save your information and to process it. Your information will only be used to pick ideas and for the possibility to get in touch with you. After the project has been completed, your data will be removed. By submitting an idea you agree that you give us the right to use and the ownership of the idea. If you have more questions, please contact us via the information below.'
     ]
 ];
--- resources/lang/nl/messages.php
+++ resources/lang/nl/messages.php
@@ -2,10 +2,14 @@
 
 return [
     'welcome' => '
-        [dutch text]
+        Je hebt hem waarschijnlijk al gezien - de regenboogbrug bij Station Delft. Een zichtbaar symbool dat Delft een regenboogstad is, en dat al bijna 10 jaar is. Maar deze brug is tijdelijk. Wij willen een permanenter LHBT+ kunstwerk, eentje uniek Delfts, en we willen jouw idee voor wat het moet worden!
+        <br /><br />
+        Tussen nu en 1 december kan je jouw idee voor een standbeeld of ander losstaand kunstwerk wat de LHBT+ gemeenschap vertegenwoordigd. Het kan een beschrijving zijn in woorden of een schets, zolang je idee maar duidelijk is. We zullen met een professioneel kunstenaar werken om het winnende idee tot een realiteit te maken.
+        <br /><br />
+        Wanneer de wedstrijd gesloten is zal een panel bestaande uit afgevaardigden van de verschillende LHBT+ organisaties in Delft het winnende idee kiezen. Dit wordt dan aangekondigd op Paarse Vrijdag, 11 december. Daarna beginnen we met een fundraising campagne om het winnende idee te financieren. De winnaar zal uitgenodigd worden om aan de verder ontwikkeling van het kunstwerk mee te doen en deel uit te maken van de uiteindelijke onthulling. Ook zal die een kleinere versie van het kunstwerk ontvangen.
     ',
     'form' => [
         'title' => 'Stuur <span class="text-art-purple">jouw idee</span> voor een <span class="text-art-purple">kunstwerk</span> in!',
-        'privacy' => 'Bij het verzenden van dit formulier geef je DWH toestemming om je gegevens op te slaan en te verwerken. Je gegevens worden alleen gebruikt voor het selecteren van ideeën to pick ideas en voor de mogelijkheid om contact met je op te nemen. Na afloop van de actie worden je gegevens verwijderd. Neem voor vragen contact op via onderstaande contactgegevens.',
+        'privacy' => 'Bij het verzenden van dit formulier geef je DWH toestemming om je gegevens op te slaan en te verwerken. Je gegevens worden alleen gebruikt voor het selecteren van ideeën to pick ideas en voor de mogelijkheid om contact met je op te nemen. Na afloop van het project worden je gegevens verwijderd. Bij het inzenden van een idee geef je ons het recht dit idee te gebruiken en draag je het idee als eigendom aan ons over. Neem voor vragen contact op via onderstaande contactgegevens.',
     ]
 ];
