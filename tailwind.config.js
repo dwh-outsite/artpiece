b52c41bc8c3aa6cb76b0a6c9db169bcb17775fc7
Casper Boone <mail@casperboone.nl>
2020-12-08 16:09:51 +0100

    Add language files to purged files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/lang/**/*.php',
     ],
 
     theme: {
